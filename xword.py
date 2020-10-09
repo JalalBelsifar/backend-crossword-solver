cf83cabf596e46c34ad8be31f2f64ccf9983d866
JalalBelsifar <jalal.belsifar@gmail.com>
2020-10-08 23:15:57 -0500

    solved the puzzle, I was not unabe to commit changes for the last two days , I recloned it

--- xword.py
+++ xword.py
@@ -2,20 +2,26 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
-
+__author__ = "Jalal, help from Rachida"
+import re
 # YOUR HELPER FUNCTION GOES HERE
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
-
-    test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    test_word = input(
+        '''Please enter a word to solve.
+        \nUse spaces to signify unknown letters: ''').lower()
 
     # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    test_word = test_word.replace(" ", ".")
+    matches = re.compile(test_word)
+    exact_length = [word for word in words if matches.match(
+        word) and len(word) == len(test_word)]
+    print(exact_length)
+    for word in exact_length:
+        print(word)
 
 
 if __name__ == '__main__':
